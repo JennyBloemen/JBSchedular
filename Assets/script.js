28617a6b5db8ca6e94a364ea05a0faa0e72af887
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-08-15 13:12:26 -0600

    finished javascript

--- Assets/script.js
+++ Assets/script.js
@@ -13,18 +13,47 @@ const userText = $(".hour-task");
 $(document).ready(function () {
     var today = moment();
     $("#currentDay").text(today.format("dddd MMMM Do YYYY, h:mm a"));
-  
+    
     var hours = today.hours();
     console.log(hours);
   });
 
-  // function to set the row colors based on the time
-  // WHEN I view the timeblocks for that day, THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-  var parent = $(this).parents(".row");
-  var timeId = parseInt(parent.attr("id"));
-  var currentHour = parseInt(moment().format("H"));
+// functin to set timeblocks for that day so that each timeblock is color coded to indicate whether it is in the past, present, or future.
+var parent = $(this).parents(".row");
+var timeId = parseInt(parent.attr("id"));
+var currentHour = parseInt(moment().format("H"));
   
-
+// sets variable for the current hour to allow the color coding
+let index = 0;
+   
+// Function to set past/present/future time.
+timeBlock.each(function () {
+    
+    var parent = $(this).parents(".row");
+    var timeId = parseInt(parent.attr("id"));
+    var currentHour = parseInt(moment().format("H"));
+    console.log(timeId);
   
+    if (timeId < currentHour) {
+    $(this).addClass("past");
   
+    } else if (timeId === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+    } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+        $(this).val(localStorage.getItem(parent.attr("id")));
+    }
+    $(this).val(localStorage.getItem(parent.attr("id")));
+})
   
+// Set .on("click") function to store the task information to local storage
+$(".saveBtn").on("click", function() {
+    var userText = $(this).siblings(".time-block").val();
+    var hourTask = $(this).parents(".row").attr("id");
+    
+    localStorage.setItem(hourTask, userText);
+    console.log("+++ Get Item from Local Storage: ", localStorage.getItem(hourTask))
+})  
